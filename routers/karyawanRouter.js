cb68a18a458ca041f738f547fcfa4c04128eb344
irvan124 <irvanmaulana124@gmail.com>
2021-09-04 13:13:50 +0700

    correction of process.env

--- routers/karyawanRouter.js
+++ routers/karyawanRouter.js
@@ -4,15 +4,15 @@ const router = express.Router();
 const { karyawanController } = require("../controllers");
 
 // GET KARYAWAN
-router.get("/", karyawanController.getKaryawan);
+router.get("/get", karyawanController.getKaryawan);
 
 // ADD KARYAWAN
-router.post("/", karyawanController.addKaryawan);
+router.post("/add", karyawanController.addKaryawan);
 
 // UPDATE KARYAWAN (PATCH)
-router.patch("/:idkaryawan", karyawanController.updateKaryawan);
+router.patch("/update:idkaryawan", karyawanController.updateKaryawan);
 
 // DELETE KARYAWAN
-router.delete("/:idkaryawan", karyawanController.deleteKaryawan);
+router.delete("/delete:idkaryawan", karyawanController.deleteKaryawan);
 
 module.exports = router;
